405f99bd0bae34f14bf977e369d15fe5bd516a0f
BigStar87 <Alexlialin@mail.ru>
2021-01-06 19:14:50 +0400

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,15 @@
 $(function(){
 
+  $('.slider__carousel').slick({
+    prevArrow: $('.slick-prev'),
+    nextArrow: $('.slick-next')
+  });
+
+  $(".rate-star").rateYo({
+    rating: 4.5,
+    starWidth: "16px",
+    readOnly: true,
+    spacing: "5px"
+  });
   
 });
