cfddfca86aedfaf1b5cbb6995a84baa64e528ea9
BigStar87 <Alexlialin@mail.ru>
2021-01-13 17:20:39 +0400

    added followers

--- app/js/main.js
+++ app/js/main.js
@@ -7,6 +7,19 @@ $(function(){
     nextArrow: $('.slick-next')
   });
 
+  var mySwiper = new Swiper('.followers__carousel', {
+    loop: true,
+    slideClass: 'followers__item',
+    wrapperClass: 'followers__inner',
+    slidesPerView: 3,
+    slidesPerGroup: 3,
+    spaceBetween: 30,
+    navigation: {
+      nextEl: '.slick-next',
+      prevEl: '.slick-prev',
+    },
+  });
+
   $(".rate-star").rateYo({
     rating: 4.5,
     starWidth: "13px",
